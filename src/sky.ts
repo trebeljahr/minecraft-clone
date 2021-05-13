73af51bf3bb8e7d6eb0dfce5ec4954b6f708089c
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2021-05-13 17:19:51 +0200

    Make day/night cycle look a lot better - but still not perfect. Probably have to write own shader for moon+stars

--- src/sky.ts
+++ src/sky.ts
@@ -23,22 +23,48 @@ export function initSky(
 
   scene.add(sky);
   const start = 180;
-  const parameters = {
-    cycle: start,
-    turbidity: 10,
-    rayleigh: 3,
-    mieCoefficient: 0.005,
+
+  const increments = {
+    rayleigh: 0.5,
+    mieDirectionalG: 0.1,
+    mieCoefficient: 0.1,
+    turbidity: 2,
+    exposure: 0.1,
+  };
+
+  const nightValues = {
+    rayleigh: 0.15,
+    mieDirectionalG: 0.05,
+    mieCoefficient: 0,
+    turbidity: 1,
+    exposure: 0.1,
+  };
+  const dayValues = {
+    rayleigh: 4,
     mieDirectionalG: 0.7,
+    mieCoefficient: 0.05,
+    turbidity: 10,
+    exposure: 0.2,
+  };
+
+  const isDay = (rotation: number) => rotation >= 180 && rotation <= 360;
+
+  const parameters = {
+    ...(isDay(start) ? dayValues : nightValues),
+    rotation: start,
     elevation: start,
     azimuth: 180,
     exposure: 0.2,
-    sunVelocity: 10,
+    sunVelocity: 20,
   };
 
   const pmremGenerator = new PMREMGenerator(renderer);
 
   function updateSun() {
-    const phi = MathUtils.degToRad(90 - parameters.elevation);
+    // if (isDay) {
+    //   phi = MathUtils.degToRad(90 - parameters.elevation - 180);
+    // }
+    let phi = MathUtils.degToRad(90 + parameters.elevation);
     const theta = MathUtils.degToRad(parameters.azimuth);
     sun.setFromSphericalCoords(1, phi, theta);
 
@@ -63,40 +89,74 @@ export function initSky(
 
   const skyGui = gui.addFolder("Sky");
   skyGui.add(parameters, "turbidity", 0.0, 20.0, 0.1).onChange(updateSky);
-  skyGui.add(parameters, "rayleigh", 0.0, 4, 0.001).onChange(updateSky);
+  skyGui.add(parameters, "rayleigh", 0.0, 10, 0.001).onChange(updateSky);
   skyGui.add(parameters, "mieCoefficient", 0.0, 0.1, 0.001).onChange(updateSky);
   skyGui.add(parameters, "mieDirectionalG", 0.0, 1, 0.001).onChange(updateSky);
-  skyGui.add(parameters, "elevation", -90, 90, 0.1).onChange(updateSky);
+  skyGui.add(parameters, "rotation", 0, 360, 0.1).onChange(updateSky);
   skyGui.add(parameters, "azimuth", -180, 180, 0.1).onChange(updateSky);
   skyGui.add(parameters, "exposure", 0, 1, 0.0001).onChange(updateSky);
-  skyGui.add(parameters, "sunVelocity", 0, 20, 0.1);
+  skyGui.add(parameters, "sunVelocity", 0, 60, 0.1);
 
   skyGui.open();
+
+  const nightfall = () =>
+    parameters.rotation >= 180 && parameters.rotation < 180 + 90;
+
+  const dayrise = () => parameters.rotation >= 0 && parameters.rotation < 90;
+
   return {
     tick: (delta: number) => {
-      if (renderer.info.render.frame % 10 === 0) {
-        console.log(parameters.exposure);
-      }
+      if (parameters.sunVelocity === 0) return;
 
-      const isNight = Math.floor(Math.abs(parameters.cycle / 180)) === 1;
-      if (isNight) {
-        parameters.elevation = parameters.cycle - 180;
-        if (parameters.rayleigh > 0.15) {
-          parameters.rayleigh -= 0.2 * delta;
+      parameters.rotation += delta * parameters.sunVelocity;
+      if (parameters.rotation >= 360) parameters.rotation = 0;
+
+      let values = dayValues;
+      let compare = (_param: string) => false;
+      let update = (_param: string, _delta: number) => {};
+      if (dayrise()) {
+        if (renderer.info.render.frame % 10 === 0) {
+          console.log("dayrise");
         }
-        if (parameters.mieCoefficient > 0.003) {
-          parameters.mieCoefficient -= 0.2 * delta;
+        compare = (param: string) => parameters[param] < values[param];
+        update = (param: string, delta: number) => {
+          parameters[param] +=
+            (increments[param] * delta * parameters.sunVelocity) / 10;
+          parameters[param] = Math.min(values[param], parameters[param]);
+        };
+      }
+      if (nightfall()) {
+        if (renderer.info.render.frame % 10 === 0) {
+          console.log("nightfall");
         }
-        if (parameters.mieDirectionalG < 0.865) {
-          parameters.mieDirectionalG += 0.2 * delta;
+        values = nightValues;
+        compare = (param: string) => parameters[param] > values[param];
+        update = (param: string, delta: number) => {
+          parameters[param] -=
+            (increments[param] * delta * parameters.sunVelocity) / 10;
+          parameters[param] = Math.max(values[param], parameters[param]);
+        };
+      }
+      if (!isDay(parameters.rotation)) {
+        if (renderer.info.render.frame % 10 === 0) {
+          // console.log("Night");
         }
+        parameters.elevation = parameters.rotation - 180;
       } else {
-        parameters.elevation = parameters.cycle;
+        if (renderer.info.render.frame % 10 === 0) {
+          // console.log("Day");
+        }
+        parameters.elevation = parameters.rotation;
       }
 
-      parameters.cycle += delta * parameters.sunVelocity;
-      if (parameters.cycle >= 360) parameters.cycle = 0;
+      Object.keys(values).forEach((param) => {
+        if (compare(param)) {
+          update(param, delta);
+        }
+      });
+
       updateSky();
+      gui.updateDisplay();
     },
   };
 }
