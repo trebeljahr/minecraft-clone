2350db5e571855e3070fb122bf8db16203407f5b
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2021-05-13 10:42:38 +0200

    Add player input class

--- src/controls.ts
+++ src/controls.ts
@@ -1,80 +1,96 @@
-export let moveForward = false;
-export let moveBack = false;
-export let moveLeft = false;
-export let moveRight = false;
-export let moveUp = false;
-export let moveDown = false;
+import { Vector3 } from "three";
 
-const onKeyDown = function (event) {
-  switch (event.code) {
-    case "ArrowUp":
-    case "KeyW":
-      moveForward = true;
-      break;
+let moveForward = false;
+let moveBack = false;
+let moveLeft = false;
+let moveRight = false;
+let moveUp = false;
+let moveDown = false;
 
-    case "ArrowLeft":
-    case "KeyA":
-      moveLeft = true;
-      break;
-
-    case "ArrowDown":
-    case "KeyS":
-      moveBack = true;
-      break;
-
-    case "ArrowRight":
-    case "KeyD":
-      moveRight = true;
-      break;
-
-    case "KeyC":
-      moveDown = true;
-      break;
-
-    case "Space":
-      moveUp = true;
-      break;
+export class PlayerInput {
+  constructor() {
+    this.addListeners();
   }
-};
 
-const onKeyUp = function (event) {
-  switch (event.code) {
-    case "ArrowUp":
-    case "KeyW":
-      moveForward = false;
-      break;
-
-    case "ArrowLeft":
-    case "KeyA":
-      moveLeft = false;
-      break;
-
-    case "ArrowDown":
-    case "KeyS":
-      moveBack = false;
-      break;
-
-    case "ArrowRight":
-    case "KeyD":
-      moveRight = false;
-      break;
-
-    case "KeyC":
-      moveDown = false;
-      break;
-
-    case "Space":
-      moveUp = false;
-      break;
+  get direction(): Vector3 {
+    const forwardBack = Number(moveForward) - Number(moveBack);
+    const leftRight = Number(moveRight) - Number(moveLeft);
+    return new Vector3(leftRight, 0, forwardBack).normalize();
   }
-};
 
-export function addListeners() {
-  console.log("Adding listeners");
+  onKeyDown(event: { code: string }) {
+    switch (event.code) {
+      case "ArrowUp":
+      case "KeyW":
+        moveForward = true;
+        break;
 
-  document.removeEventListener("keydown", onKeyDown);
-  document.removeEventListener("keyup", onKeyUp);
+      case "ArrowLeft":
+      case "KeyA":
+        moveLeft = true;
+        break;
 
-  document.addEventListener("keydown", onKeyDown);
-  document.addEventListener("keyup", onKeyUp);
+      case "ArrowDown":
+      case "KeyS":
+        moveBack = true;
+        break;
+
+      case "ArrowRight":
+      case "KeyD":
+        moveRight = true;
+        break;
+
+      case "KeyC":
+        moveDown = true;
+        break;
+
+      case "Space":
+        moveUp = true;
+        break;
+    }
+  }
+
+  onKeyUp(event: { code: string }) {
+    switch (event.code) {
+      case "ArrowUp":
+      case "KeyW":
+        moveForward = false;
+        break;
+
+      case "ArrowLeft":
+      case "KeyA":
+        moveLeft = false;
+        break;
+
+      case "ArrowDown":
+      case "KeyS":
+        moveBack = false;
+        break;
+
+      case "ArrowRight":
+      case "KeyD":
+        moveRight = false;
+        break;
+
+      case "KeyC":
+        moveDown = false;
+        break;
+
+      case "Space":
+        moveUp = false;
+        break;
+    }
+  }
+
+  addListeners() {
+    console.log("Adding listeners");
+    document.addEventListener("keydown", this.onKeyDown);
+    document.addEventListener("keyup", this.onKeyUp);
+  }
+
+  removeListeners() {
+    console.log("Removing listeners");
+    document.removeEventListener("keydown", this.onKeyDown);
+    document.removeEventListener("keyup", this.onKeyUp);
+  }
 }
